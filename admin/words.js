17207e8e10d8f1f3bbd7d49306cc61f084314426
Mic-M <iob.micm@gmail.com>
2020-05-26 19:10:37 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'smartcontrol adapter settings': {
+        'en': 'Adapter settings for smartcontrol',
+        'de': 'Adaptereinstellungen für smartcontrol',
+        'ru': 'Настройки адаптера для smartcontrol',
+        'pt': 'Configurações do adaptador para smartcontrol',
+        'nl': 'Adapterinstellingen voor smartcontrol',
+        'fr': "Paramètres d'adaptateur pour smartcontrol",
+        'it': "Impostazioni dell'adattatore per smartcontrol",
+        'es': 'Ajustes del adaptador para smartcontrol',
+        'pl': 'Ustawienia adaptera dla smartcontrol',
+        'zh-cn': 'smartcontrol的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
